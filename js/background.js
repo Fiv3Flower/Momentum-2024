4aac1f52936a71a580e0579dbcc711f202c01442
Seojun <brave605@naver.com>
2024-04-17 23:04:00 +0900

    Javascript working done!

---
+++ js/background.js
@@ -0,0 +1,14 @@
+const images = ["gayoii.1.jpg","gayoii.2.jpeg","gayoii.3.jpeg"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+
+bgImage.src = `img/${chosenImage}`;
+
+// document.body.appendChild(bgImage);
+
+// When you finish CSS work, apply it!
+
+// const bgImage = `url(img/${chosenImage})` 
+// document.body.style.backgroundImage = bgImage
---
+++ js/weather.js
@@ -0,0 +1,22 @@
+const API_KEY = "25e0348d29ed852e59ef72bd5a92db24";
+
+function onGeoOk(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+        .then(Response => Response.json())
+        .then((data) => {
+        const degree = document.querySelector("#weather span:nth-child(2)");
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        degree.innerText = `/ ${data.main.temp}°C`;
+        city.innerText = data.name;
+        weather.innerText = data.weather[0].main;
+    });
+}
+function onGeoError() {
+    alert("Can't find you. No weather for you");
+}
+
+// navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
