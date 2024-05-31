f8198e788b55c4ed8ee36041b3c386a0d5f91f99
Seojun <brave605@naver.com>
2024-05-31 13:35:56 +0900

    Publishing

--- js/background.js
+++ js/background.js
@@ -1,13 +1,6 @@
-const images = ["gayoii.1.jpg"];//,"gayoii.2.jpg","gayoii.3.jpeg"];
+const images = ["main_visual2.jpg"];//,"gayoii.2.jpg","gayoii.3.jpeg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
-
-//const bgImage = document.createElement("img");
-//bgImage.src = `img/${chosenImage}`;
-
-// document.body.appendChild(bgImage);
-
-// When you finish CSS work, apply it!
 
 const bgImage = `url(img/${chosenImage})` 
 document.body.style.backgroundImage = bgImage
